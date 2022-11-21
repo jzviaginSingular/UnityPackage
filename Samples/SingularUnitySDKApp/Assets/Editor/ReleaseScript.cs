96ab2e379e7c618cba9b8f8e7e42b40a35c2c8d1
jzviaginSingular <100220215+jzviaginSingular@users.noreply.github.com>
2022-11-21 18:50:57 +0200

    added sample app

---
+++ Samples/SingularUnitySDKApp/Assets/Editor/ReleaseScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ReleaseScript {
+
+    [MenuItem("Assets/Build Package")]
+    public static void BuildPackage() {
+        AssetDatabase.ExportPackage(GetAssets().ToArray(), "temp-singular-sdk.unitypackage");
+    }
+
+    private static List<string> GetAssets() {
+        List<string> assests = new List<string>();
+
+        string[] a = AssetDatabase.GetAllAssetPaths();
+        Array.Sort(a);
+        assests = a.ToList().Where(x => (x.StartsWith("Assets/Plugins/") || x.StartsWith("Assets/Code/")) &&
+        !(x.EndsWith("Main.cs") || x.EndsWith("ReleaseScript.cs"))).ToList();
+
+        return assests;
+    }
+
+}
+
